52288dc593fb34579fe00967c0a554c2c1da8b0b
IVDgenomicslab <67246240+IVDgenomicslab@users.noreply.github.com>
2020-08-11 20:38:14 -0500

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,68 @@
+
+#' @title Muraro pancreas data: A SingleCellExperiment with celltypes in the "celltypes" colData.
+#' @description A subset of the Melanoma data, from Muraro et al. (2016),  Cell Syst.
+#'
+#' @docType data
+#' @format This is a SingleCellExperiment
+#' @references Muraro et al. (2016),  Cell Syst.
+#'
+#' @source for the original data: \href{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE85241}{GEO}
+"muraro_test"
+
+#' @title Xin pancreas data: A SingleCellExperiment with celltypes in the "celltypes" colData.
+#' @description A subset of the Melanoma data , from Xin et al. (2016) Cell Metab.
+#'
+#' @docType data
+#' @format This is a SingleCellExperiment
+#' @references Xin et al. (2016) Cell Metab.
+#'
+#' @source for the original data: \href{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE81608}{GEO}
+"xin_test"
+
+
+
+#' @title Melanoma reference data: A SingleCellExperiment with celltypes in the "celltypes" colData.
+#' @description A subset of the Melanoma data (without tumor cells), from Tirosh et al. (2016), Science.
+#'
+#' @docType data
+#' @format This is a SingleCellExperiment
+#' @references Tirosh et al. (2016) Science 6282:189-196
+#'
+#' @source for the original data: \href{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE72056}{GEO}
+"melanoma_ref"
+
+#' @title A SingleCellExperiment with Melanoma test data.
+#' @description A subset of the Melanoma data, from Tirosh et al. (2016), Science.
+#'
+#' @docType data
+#' @format This is a SingleCellExperiment
+#' @references Tirosh et al. (2016) Science 6282:189-196
+#'
+#' @source for the original data: \href{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE72056}{GEO}
+"melanoma_test"
+
+
+
+
+
+#' @title Gene position used for CNV analysis in tumor cells
+#' @description A data frame object of gene position with four colums (Chromosome, Start_postition, End_position, Gene_symbol).
+#'
+#' @docType data
+#' @format This is a list
+#'
+"gene_position_file"
+
+
+
+#' @title Gene list used for calculate epithelial score
+#' @description A list object with the epithelial related genes used for epithelial score calculation.
+#'
+#' @docType data
+#' @format This is a data frame
+#'
+"gene_list"
+
+
+
+
